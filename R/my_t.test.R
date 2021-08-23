92d8730a2806a3a0753e466d86442fbdd5b0b560
Rolina Cao <rolinachr@gmail.com>
2021-08-22 20:46:23 -0700

    Edited

--- R/my_t.test.R
+++ R/my_t.test.R
@@ -21,27 +21,27 @@
 #'
 #' @export
 #'
-#' my_t.test <- function(x, alternative, mu) {
-# calculate t value
-test_stat <- (mean(x) - mu) / (sd(x) / sqrt(length(x)))
-# calculate degree of freedom
-df <- length(x) - 1
-# calculate p-value or one tail that could be less or greater
-if (alternative == "two.sided") {
-  # two sides
-  p_val <- pt(abs(test_stat), df, lower.tail = FALSE)
-  p_val <- p_val * 2
-} else if (alternative == "less") {
-  # lower tail
-  p_val <- pt(test_stat, df, lower.tail = TRUE)
-} else if (alternative == "greater") {
-  # upper tail
-  p_val <- pt(abs(test_stat), df, lower.tail = FALSE)
-} else {
-  # error when input is not 'two.sided', 'less', or 'greater'
-  stop("alternative should only accept 'two.sided', 'less', or 'greater'!")
-}
-# create a list
-result <- list(test_stat, df, alternative, p_val)
-return(result)
+my_t.test <- function(x, alternative, mu) {
+  # calculate t value
+  test_stat <- (mean(x) - mu) / (sd(x) / sqrt(length(x)))
+  # calculate degree of freedom
+  df <- length(x) - 1
+  # calculate p-value or one tail that could be less or greater
+  if (alternative == "two.sided") {
+    # two sides
+    p_val <- pt(abs(test_stat), df, lower.tail = FALSE)
+    p_val <- p_val * 2
+  } else if (alternative == "less") {
+    # lower tail
+    p_val <- pt(test_stat, df, lower.tail = TRUE)
+  } else if (alternative == "greater") {
+    # upper tail
+    p_val <- pt(abs(test_stat), df, lower.tail = FALSE)
+  } else {
+    # error when input is not 'two.sided', 'less', or 'greater'
+    stop("alternative should only accept 'two.sided', 'less', or 'greater'!")
+  }
+  # create a list
+  result <- list(test_stat, df, alternative, p_val)
+  return(result)
 }
