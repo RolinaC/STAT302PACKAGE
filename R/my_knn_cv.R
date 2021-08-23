6842bd2cf9346a7fbafc899b64f4628fbaa114f7
Rolina Cao <rolinachr@gmail.com>
2021-08-22 21:56:54 -0700

    edit

--- R/my_knn_cv.R
+++ R/my_knn_cv.R
@@ -21,43 +21,6 @@
 #'
 #' @export
 
-library(class)
-library(tidyverse)
-library(palmerpenguins)
-library(randomForest)
-
-my_knn_cv <- function(train, cl, k_nn, k_cv) {
-  # split data into k_cv folds, randomly with equal probability
-  split <- sample(rep(1:k_cv, length = nrow(train)))
-  data <- data.frame("train" = train, "cl" = cl, "split" = split)
-
-  mis <- c()
-
-  #Iterate to predict
-  for (i in 1:k_cv) {
-    # create training and testing data
-    data_train <- filter(data, split != i)
-    data_test <- filter(data, split == i)
-
-    # select train and test
-    train_selected <- data_train %>% select(-split, -cl)
-    test_selected <- data_test %>% select(-split, -cl)
-
-    cl <- data_train %>% pull(cl)
-
-    # k-nearst-neighbors
-    pred_class <- knn(train = train_selected,
-                      test = test_selected,
-                      cl = cl,
-                      k = k_nn)
-    # misclassification error
-    mis[i] <- mean(ifelse(data_test$cl == pred_class, 0, 1))
-  }
-  cv_err <- mean(mis)
-  result <- list("class" = pred_class, "cv_err" = cv_err)
-  return(result)
-}
-
 my_knn_cv <- function(train, cl, k_nn, k_cv) {
   # fold into k_cv
   fold <- base::sample(base::rep(1:k_cv, length = nrow(train)))
